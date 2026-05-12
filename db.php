49ff1dce231102e75094a739aa42c097e8603eba
cramskie <marcgerona19@gmail.com>
2026-05-12 14:10:23 +0800

    Debug db connection

--- db.php
+++ db.php
@@ -10,7 +10,7 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
-    die("Database Connection Error: " . $e->getMessage());
+    die("Error: host=" . $host . " db=" . $dbname . " user=" . $user . " port=" . $port . " | " . $e->getMessage());
 }
 
 session_start();
